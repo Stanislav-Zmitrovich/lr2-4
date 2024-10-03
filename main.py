a8727d5b5a887c52a1f9205157d4d3ae1b1039d3
stasik <stanislavzmitrovic3@gmail.com>
2024-10-03 10:08:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
 import math
-
-def calculate_psi(x, y):
-    term1 = x  (y / x)
-    term2 = (y / math.sqrt(x))  (1/3)
-    psi = abs(term1 - term2) + (y - x)
-    return psi
-x = float(input("Введите значение x: "))
-y = float(input("Введите значение y: "))
-psi_value = calculate_psi(x, y)
-print(f"Значение функции ψ: {psi_value}")
+x = 5
+y = 10
+power_term = x  (y / x)
+root_term = (y / x)  (1 / 3)
+abs_difference = abs(power_term - root_term)
+psi = abs_difference + (y - x)
+print(f"Значение функции psi для x={x} и y={y} равно {psi}")
